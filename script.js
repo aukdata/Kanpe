50792d2561289e4285fd7a51ba176f3db0224ad4
takuma <takuma.y0524@gmail.com>
2018-03-16 20:50:43 +0900

    Fix: design

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//(() => {
+(() => {
 	function formatTime(milisecond) {
 		let second = Math.floor(Math.abs(milisecond) / 1000) % 60
 		let minute = Math.floor(Math.abs(milisecond) / 1000 / 60) % 60
@@ -9,10 +9,6 @@
 			(minute > 0 ? minute + "m " : "") + second + "s"
 	}
 
-	function sum(arr) {
-		return arr.reduce((a, b) => a + b)
-	}
-
 	let section_timer = new Timer()
 	let part_timer = new Timer()
 	let part_lap_elapsed_time = []
@@ -21,166 +17,175 @@
 	let parts = null
 	let index = 0
 
-	document.addEventListener("DOMContentLoaded", () => {
-		const element_section_title = document.getElementById("section_title")
+	const element_section_title = document.getElementById("section_title")
 
-		const element_section_elapsed_time_bar = document.getElementById("section_elapsed_time_bar")
-		const element_section_remaining_time_bar = document.getElementById("section_remaining_time_bar")
+	const element_section_elapsed_time_bar = document.getElementById("section_elapsed_time_bar")
+	const element_section_remaining_time_bar = document.getElementById("section_remaining_time_bar")
 
-		const element_section_elapsed_time = document.getElementById("section_elapsed_time")
-		const element_section_remaining_time = document.getElementById("section_remaining_time")
+	const element_section_elapsed_time = document.getElementById("section_elapsed_time")
+	const element_section_remaining_time = document.getElementById("section_remaining_time")
 
-		const element_gap_time = document.getElementById("gap_time")
-
-
-		const element_present_part_contents = document.getElementById("present_part_contents")
-
-		const element_present_part_elapsed_time_bar = document.getElementById("present_part_elapsed_time_bar")
-		const element_present_part_remaining_time_bar = document.getElementById("present_part_remaining_time_bar")
-
-		const element_present_part_elapsed_time = document.getElementById("present_part_elapsed_time")
-		const element_present_part_remaining_time = document.getElementById("present_part_remaining_time")
-
-		const element_next_part_contents = document.getElementById("next_part_contents")
+	const element_gap_time = document.getElementById("gap_time")
 
 
-		// When json file loaded
-		document.getElementById("file_selector").addEventListener("change", e => {
-			const f = e.target.files[0]
-			const reader = new FileReader()
-			reader.readAsText(f)
-			reader.addEventListener("load", () => {
-				const data = JSON.parse(reader.result)
-				section_title = data.title
-				parts = data.parts
+	const element_present_part_contents = document.getElementById("present_part_contents")
 
-				element_section_title.textContent = section_title
-				initialize()
-			})
-		})
+	const element_present_part_elapsed_time_bar = document.getElementById("present_part_elapsed_time_bar")
+	const element_present_part_remaining_time_bar = document.getElementById("present_part_remaining_time_bar")
 
-		// When screen is tappped
-		document.addEventListener("click", () => {
-			nextPart()
-		})
+	const element_present_part_elapsed_time = document.getElementById("present_part_elapsed_time")
+	const element_present_part_remaining_time = document.getElementById("present_part_remaining_time")
 
-		// Update every 100ms
-		window.setInterval(() => {
-			updateTime()
-		}, 100)
+	const element_next_part_contents = document.getElementById("next_part_contents")
 
-
-		function initialize() {
-			calcEndTime()
-
-			section_timer.start()
-			part_timer.start()
-			pause()
-
-			index = 0
-			part_lap_elapsed_time = []
-
-			updatePart()
-			updateTime()
-		}
-
-		function calcEndTime() {
-			let accumlation = 0
-			for (part of parts) {
-				accumlation += part.period
-				part.until = accumlation * 60 * 1000
-				part.periodms = part.period * 60 * 1000
-			}
-		}
-
-		function updatePart() {
-			if (parts != null) {
-				element_present_part_contents.textContent = index < parts.length ? parts[index].contents : "───"
-				element_next_part_contents.textContent = index + 1 < parts.length ? parts[index + 1].contents : "───"
-			} else {
-				element_present_part_contents.textContent = "───"
-				element_next_part_contents.textContent = "───"
-			}
-		}
-
-		function updateTime() {
-			if (parts == null) return
-
-			let elapsed_time = section_timer.milliseconds()
-			let remaining_time = parts[parts.length - 1].until - elapsed_time
-			updateGraph(element_section_elapsed_time, element_section_remaining_time,
-				element_section_elapsed_time_bar, element_section_remaining_time_bar,
-				elapsed_time, remaining_time)
-
-
-			let gap_period = 0
-			let elapsed_time_till_prev_part = index - 1 >= 0 ? part_lap_elapsed_time[index - 1] : 0
-			let planned_elapsed_time_till_prev_part = index - 1 >= 0 ? parts[index - 1].until : 0
-			let present_part_ending_elapsed_time = elapsed_time_till_prev_part + parts[index].periodms
-
-			if (elapsed_time > present_part_ending_elapsed_time) {
-				gap_period = elapsed_time_till_prev_part - planned_elapsed_time_till_prev_part + elapsed_time - present_part_ending_elapsed_time
-			} else {
-				gap_period = elapsed_time_till_prev_part - planned_elapsed_time_till_prev_part
-			}
-			element_gap_time.textContent = formatTime(gap_period)
-
-
-			let present_part_elapsed_time = part_timer.milliseconds()
-			let present_part_remaining_time = parts[index].periodms - present_part_elapsed_time
-			updateGraph(element_present_part_elapsed_time, element_present_part_remaining_time,
-				element_present_part_elapsed_time_bar, element_present_part_remaining_time_bar,
-				present_part_elapsed_time, present_part_remaining_time)
-		}
-
-		function updateGraph(element_elapsed_text, element_remaining_text, element_elapsed_bar, element_remaining_bar, elapsed, remaining) {
-			element_elapsed_text.textContent = formatTime(elapsed)
-			element_remaining_text.textContent = formatTime(remaining)
-
-			if (remaining >= 0) {
-				element_elapsed_bar.style.width = "" + (100 * elapsed / (remaining + elapsed)) + "%"
-				element_remaining_bar.style.width = "" + (100 * remaining / (remaining + elapsed)) + "%"
-			} else {
-				element_elapsed_bar.style.width = "100%"
-				element_remaining_bar.style.width = "0%"
-			}
-		}
-
-		function nextPart() {
-			if (parts == null) return
-
-			part_lap_elapsed_time.push(section_timer.milliseconds())
-
-			if (index < parts.length) {
-				part_timer.start()
-				if (section_timer.isPausing()) {
-					part_timer.pause()
-				}
-			} else {
-				updateTime()
-				parts = null
-			}
-
-			index++
-			updatePart()
-		}
-	})
 
 	const element_pause_button = document.getElementById("pause_button")
 	const element_pause_image = document.getElementById("pause_image")
 
-	element_pause_button.addEventListener("click", e => {
-		if (section_timer.isPausing()) {
-			remuse()
+
+	// When json file loaded
+	document.getElementById("file_selector").addEventListener("change", e => {
+		const f = e.target.files[0]
+		const reader = new FileReader()
+		reader.readAsText(f)
+		reader.addEventListener("load", () => {
+			const data = JSON.parse(reader.result)
+			section_title = data.title
+			parts = data.parts
+
+			element_section_title.textContent = section_title
+			initializeSection()
+		})
+	})
+
+	// When screen is tappped
+	document.addEventListener("click", () => {
+		nextPart()
+	})
+
+	// Update every 100ms
+	window.setInterval(() => {
+		updateTime()
+	}, 100)
+
+
+	function initializeSection() {
+		calcEndTime()
+
+		section_timer.start()
+		part_timer.start()
+		pause()
+
+		index = 0
+		part_lap_elapsed_time = []
+
+		updatePart()
+		updateTime()
+
+		element_pause_button.style.height = "80px"
+	}
+
+	function calcEndTime() {
+		let accumlation = 0
+		for (part of parts) {
+			accumlation += part.period
+			part.until = accumlation * 60 * 1000
+			part.periodms = part.period * 60 * 1000
+		}
+	}
+
+	function updatePart() {
+		if (parts != null) {
+			element_present_part_contents.textContent = index < parts.length ? parts[index].contents : "───"
+			element_next_part_contents.textContent = index + 1 < parts.length ? parts[index + 1].contents : "───"
 		} else {
-			pause()
+			element_present_part_contents.textContent = "───"
+			element_next_part_contents.textContent = "───"
+		}
+	}
+
+	function updateTime() {
+		if (parts == null) return
+
+		let elapsed_time = section_timer.milliseconds()
+		let remaining_time = parts[parts.length - 1].until - elapsed_time
+		updateGraph(element_section_elapsed_time, element_section_remaining_time,
+			element_section_elapsed_time_bar, element_section_remaining_time_bar,
+			elapsed_time, remaining_time)
+
+
+		let gap_period = 0
+		let elapsed_time_till_prev_part = index - 1 >= 0 ? part_lap_elapsed_time[index - 1] : 0
+		let planned_elapsed_time_till_prev_part = index - 1 >= 0 ? parts[index - 1].until : 0
+		let present_part_ending_elapsed_time = elapsed_time_till_prev_part + parts[index].periodms
+
+		if (elapsed_time > present_part_ending_elapsed_time) {
+			gap_period = elapsed_time_till_prev_part - planned_elapsed_time_till_prev_part + elapsed_time - present_part_ending_elapsed_time
+		} else {
+			gap_period = elapsed_time_till_prev_part - planned_elapsed_time_till_prev_part
+		}
+		element_gap_time.textContent = (gap_period == 0 ? "± " : (gap_period < 0 ? "- " : "+ ")) + formatTime(Math.abs(gap_period))
+
+
+		let present_part_elapsed_time = part_timer.milliseconds()
+		let present_part_remaining_time = parts[index].periodms - present_part_elapsed_time
+		updateGraph(element_present_part_elapsed_time, element_present_part_remaining_time,
+			element_present_part_elapsed_time_bar, element_present_part_remaining_time_bar,
+			present_part_elapsed_time, present_part_remaining_time)
+	}
+
+	function updateGraph(element_elapsed_text, element_remaining_text, element_elapsed_bar, element_remaining_bar, elapsed, remaining) {
+		element_elapsed_text.textContent = formatTime(elapsed)
+		element_remaining_text.textContent = formatTime(remaining)
+
+		if (remaining >= 0) {
+			element_elapsed_bar.style.width = "" + (100 * elapsed / (remaining + elapsed)) + "%"
+			element_remaining_bar.style.width = "" + (100 * remaining / (remaining + elapsed)) + "%"
+		} else {
+			element_elapsed_bar.style.width = "100%"
+			element_remaining_bar.style.width = "0%"
+		}
+	}
+
+	function nextPart() {
+		if (parts == null) return
+
+		part_lap_elapsed_time.push(section_timer.milliseconds())
+
+		if (index + 1 < parts.length) {
+			part_timer.start()
+			if (section_timer.isPausing()) {
+				part_timer.pause()
+			}
+		} else {
+			finishSection()
+		}
+
+		index++
+		updatePart()
+	}
+
+	function finishSection() {
+		element_pause_button.style.height = "0px"
+		updateTime()
+		parts = null
+	}
+
+	element_pause_button.addEventListener("click", e => {
+		if (parts != null) {
+			if (section_timer.isPausing()) {
+				remuse()
+			} else {
+				pause()
+			}
 		}
 		e.stopPropagation()
 	})
-	
+
 	function pause() {
 		section_timer.pause()
 		part_timer.pause()
+
 		element_pause_button.style.background = "limegreen"
 		element_pause_image.src = "./assets/remuse.png"
 		element_pause_image.alt = "Remuse"
@@ -189,8 +194,9 @@
 	function remuse() {
 		section_timer.remuse()
 		part_timer.remuse()
+
 		element_pause_button.style.background = "red"
 		element_pause_image.src = "./assets/pause.png"
 		element_pause_image.alt = "Pause"
 	}
-//})()
+})()
